ad3ffd66bd8786836f1ce4b56eca39192f2f8a53
AhmadQureshi-cloud <58387304+AhmadQureshi-cloud@users.noreply.github.com>
2021-01-21 19:21:33 +0500

    Add files via upload

---
+++ admin-service/src/main/java/com/filecloud/adminservice/controller/AdminController.java
@@ -0,0 +1,84 @@
+package com.filecloud.adminservice.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.filecloud.adminservice.model.dto.SingleIdRequestDto;
+import com.filecloud.adminservice.response.Result;
+import com.filecloud.adminservice.security.role.Admin;
+import com.filecloud.adminservice.service.AdminService;
+
+
+@Admin
+@RequestMapping("api/v1/")
+@RestController
+public class AdminController extends BaseController {
+
+	private final AdminService adminService;
+
+	@Autowired
+	public AdminController(AdminService adminService) {
+		this.adminService = adminService;
+	}
+
+	@GetMapping("list-users")
+	public Result<?> listUsers() {
+		return adminService.listUsers();
+	}
+
+	@GetMapping("/list-active-users")
+	public Result<?> activeUsers() {
+		return adminService.activeUsers();
+	}
+
+	@GetMapping("/list-inactive-users")
+	public Result<?> inactiveusers() {
+		return adminService.inactiveusers();
+	}
+
+	@GetMapping("get-user/{userId}")
+	public Result<?> getUser(@PathVariable long userId) {
+		return adminService.getUser(userId);
+	}
+
+	@PostMapping("enable-user")
+	public Result<?> enableUser(@RequestBody SingleIdRequestDto dto) {
+		return adminService.enableUser(dto);
+	}
+
+	@PostMapping("disable-user")
+	public Result<?> disableUser(@RequestBody SingleIdRequestDto dto) {
+		return adminService.disableUser(dto);
+	}
+
+	@PostMapping("delete-user")
+	public Result<?> deleteUser(@RequestBody SingleIdRequestDto dto) {
+		return adminService.deleteUser(dto);
+	}
+
+	@GetMapping("/inactive-users-count")
+	public Result<?> inActiveUserCount() {
+		return adminService.getInActiveUserCount();
+	}
+
+	@GetMapping("/all-users-count")
+	public Result<?> allUsersCount() {
+		return adminService.getAllUsersCount();
+	}
+
+	@GetMapping("/list-role-admin")
+	public Result<?> listRoleAdmin() {
+		return adminService.listRoleAdmin();
+	}
+
+	@GetMapping("/list-role-user")
+	public Result<?> listRoleUser() {
+		return adminService.listRoleUser();
+	}
+
+}
---
+++ admin-service/src/main/java/com/filecloud/adminservice/service/AdminService.java
@@ -0,0 +1,78 @@
+package com.filecloud.adminservice.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.filecloud.adminservice.client.AuthServerClient;
+import com.filecloud.adminservice.client.DocumentServiceClient;
+import com.filecloud.adminservice.model.dto.SingleIdRequestDto;
+import com.filecloud.adminservice.response.Response.Status;
+import com.filecloud.adminservice.response.Result;
+import com.filecloud.adminservice.security.util.AuthUtil;
+
+
+@Service
+public class AdminService extends BaseService {
+
+	private final AuthServerClient authClient;
+
+	private final DocumentServiceClient documentClient;
+
+	@Autowired
+	public AdminService(AuthServerClient authClient, DocumentServiceClient documentClient) {
+		this.authClient = authClient;
+		this.documentClient = documentClient;
+	}
+
+	public Result<?> listUsers() {
+		return authClient.listUsers(AuthUtil.getBearerToken());
+	}
+
+	public Result<?> activeUsers() {
+		return authClient.activeUsers(AuthUtil.getBearerToken());
+	}
+
+	public Result<?> inactiveusers() {
+		return authClient.inactiveusers(AuthUtil.getBearerToken());
+	}
+
+	public Result<?> getUser(long userId) {
+		return authClient.getUser(AuthUtil.getBearerToken(), userId);
+	}
+
+	public Result<?> enableUser(SingleIdRequestDto dto) {
+		return authClient.enableUser(AuthUtil.getBearerToken(), dto);
+	}
+
+	public Result<?> disableUser(SingleIdRequestDto dto) {
+		return authClient.disableUser(AuthUtil.getBearerToken(), dto);
+	}
+
+	public Result<?> deleteUser(SingleIdRequestDto dto) {
+		Result<?> result = authClient.deleteUser(AuthUtil.getBearerToken(), dto);
+
+		if (result.isSuccess() && result.getStatusCode() == Status.ALL_OK.getStatusCode())
+			documentClient.deleteUserDocuments(AuthUtil.getBearerToken(), dto);
+		else
+			error(result.getMessage());
+
+		return result;
+	}
+
+	public Result<?> getInActiveUserCount() {
+		return authClient.inActiveUserCount(AuthUtil.getBearerToken());
+	}
+
+	public Result<?> getAllUsersCount() {
+		return authClient.allUsersCount(AuthUtil.getBearerToken());
+	}
+
+	public Result<?> listRoleAdmin() {
+		return authClient.listRoleAdmin(AuthUtil.getBearerToken());
+	}
+
+	public Result<?> listRoleUser() {
+		return authClient.listRoleUser(AuthUtil.getBearerToken());
+	}
+
+}
